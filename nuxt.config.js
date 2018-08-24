36bd9cd4a92ea68c9438ba0cc6e6a7aa97883c8d
Cyriac Thomas <me@cyriacthomas.com>
2018-08-24 15:20:03 +0530

    added shards-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,12 @@ module.exports = {
   */
   head: {
     title: 'afterfloodinfographics',
+    script :[
+      { src:"https://code.jquery.com/jquery-3.2.1.slim.min.js", integrity:"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN", crossorigin:"anonymous" },
+      { src:"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", integrity:"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q", crossorigin:"anonymous" },
+      { src:"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js", integrity:"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl", crossorigin:"anonymous" },
+      { src:"https://unpkg.com/shards-ui@2.0.3/dist/js/shards.min.js" }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -27,7 +33,9 @@ module.exports = {
       { rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png" },
       { rel:"icon", type:"image/png", sizes:"96x96", href:"/favicon-96x96.png" },
       { rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png" },
-      { rel:"manifest", href:"/manifest.json" }
+      { rel:"manifest", href:"/manifest.json" },
+      { rel:"stylesheet", href:"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", integrity:"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm", crossorigin:"anonymous" },
+      { rel:"stylesheet", href:"https://unpkg.com/shards-ui@2.0.3/dist/css/shards.min.css"}
     ]
   },
   /*
