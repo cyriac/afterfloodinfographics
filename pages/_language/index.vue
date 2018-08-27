4f06b5807c131e8bb93f0c928919a4b822e2f5e9
Cyriac Thomas <me@cyriacthomas.com>
2018-08-27 13:44:34 +0530

    moved card columns to bootstrap-vue

--- pages/_language/index.vue
+++ pages/_language/index.vue
@@ -11,30 +11,32 @@
           </b-col>
         </b-row>
       </b-col>
-      <b-col cols="12" class="card-columns">
-        <nuxt-link :to="'/' + $route.params.language + '/' + doc['slug']" v-if="documents.length > 0" v-for="doc in documents" :key="doc.slug">
-          <b-card :img-src="getimgurl(doc[$route.params.language + ':png'])" img-bottom v-if="doc[$route.params.language + ':png'] !== null" class="card-no-title">
+      <b-col cols="12">
+        <b-card-group columns>
+          <nuxt-link :to="'/' + $route.params.language + '/' + doc['slug']" v-if="documents.length > 0" v-for="doc in documents" :key="doc.slug">
+            <b-card :img-src="getimgurl(doc[$route.params.language + ':png'])" img-bottom v-if="doc[$route.params.language + ':png'] !== null" class="card-no-title">
 
-            <div class="card-footer" @click.native="preventDefault">
-            <div class="row">
-              <div class="col">
-                <p class="share-info">Share infographic</p>
-              </div>
-              <div class="col align-right">
-                <a class="sm-share-small" :href="'https://www.facebook.com/sharer.php?caption=After Flood Infographics on' + doc['Project Description'] + '&description=After Flood Infographics on' + doc['Project Description'] + '&u=https://infographics.afterflood.in/#/' + $route.params.language + '/' + doc['slug']" target="_blank">
-                  <img src="https://res.cloudinary.com/dck5rpuat/image/upload/v1535339958/fb-icon-coloured.png">
-                </a>
-                <a class="sm-share-small" :href="'https://twitter.com/share?text=After Flood Infographics on' + doc['Project Description'] + '&hashtags=AfterFlood, Infographics, KeralaFloods' + '&url=https://infographics.afterflood.in/#/' + $route.params.language + '/' + doc['slug']" target="_blank">
-                  <img src="https://res.cloudinary.com/dck5rpuat/image/upload/v1535339958/twitter-icon-coloured.png">
-                </a>
+              <div class="card-footer" @click.native="preventDefault">
+              <div class="row">
+                <div class="col">
+                  <p class="share-info">Share infographic</p>
+                </div>
+                <div class="col align-right">
+                  <a class="sm-share-small" :href="'https://www.facebook.com/sharer.php?caption=After Flood Infographics on' + doc['Project Description'] + '&description=After Flood Infographics on' + doc['Project Description'] + '&u=https://infographics.afterflood.in/#/' + $route.params.language + '/' + doc['slug']" target="_blank">
+                    <img src="https://res.cloudinary.com/dck5rpuat/image/upload/v1535339958/fb-icon-coloured.png">
+                  </a>
+                  <a class="sm-share-small" :href="'https://twitter.com/share?text=After Flood Infographics on' + doc['Project Description'] + '&hashtags=AfterFlood, Infographics, KeralaFloods' + '&url=https://infographics.afterflood.in/#/' + $route.params.language + '/' + doc['slug']" target="_blank">
+                    <img src="https://res.cloudinary.com/dck5rpuat/image/upload/v1535339958/twitter-icon-coloured.png">
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
 
-          </b-card>
-          <b-card :title="doc['Project Description']" v-else></b-card>
-          
-        </nuxt-link>
+            </b-card>
+            <b-card :title="doc['Project Description']" v-else></b-card>
+
+          </nuxt-link>
+        </b-card-group>
       </b-col>
     </b-row>
   </transition>
@@ -136,7 +138,7 @@ export default {
   .sm-share-small{
     display: inline-block;
     margin-right: 15px;
-    
+
     img {
       max-width: 28px;
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <transition name="fade">
   <b-row class="text-center">
-    <b-col cols="12" class="card-columns">
-      <nuxt-link v-if="$store.state.languages" v-for="lang in $store.state.languages" :key="lang" :to="lang">
-        <b-card class="lang-card" :title="lang">
-        	<img class="lang-img" :src="'https://res.cloudinary.com/dck5rpuat/image/upload/v1535347620/letters/' + lang.toLowerCase().trim() +'.png'">
-        </b-card>
-      </nuxt-link>
+    <b-col cols="12">
+      <b-card-group columns>
+        <nuxt-link v-if="$store.state.languages" v-for="lang in $store.state.languages" :key="lang" :to="lang">
+          <b-card class="lang-card" :title="lang">
+          	<img class="lang-img" :src="'https://res.cloudinary.com/dck5rpuat/image/upload/v1535347620/letters/' + lang.toLowerCase().trim() +'.png'">
+          </b-card>
+        </nuxt-link>
+      </b-card-group>
     </b-col>
   </b-row>
   </transition>
@@ -27,18 +29,17 @@ export default{
 	height: 150px;
 	.card-body{
 		position:relative;
-
 		.lang-img{
 			position:absolute;
-	     	right: 20px;
-    		top: 20px;
-    		opacity: 0.5;
+	    right: 20px;
+    	top: 20px;
+    	opacity: 0.5;
 		}
 		.card-title{
 			text-align: left;
 			margin-top:40px;
 			padding-left: 10px;
 		}
-	}	
+	}
 }
 </style>
