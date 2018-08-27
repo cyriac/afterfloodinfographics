66dc6928b513d3f1a866a85b5f2aa6042525f08d
Anoop Sethumadhavan <anoop4sethu@gmail.com>
2018-08-27 11:06:35 +0530

    Updated language cards with letter previews

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,41 @@
   <b-row class="text-center">
     <b-col cols="12" class="card-columns">
       <nuxt-link v-if="$store.state.languages" v-for="lang in $store.state.languages" :key="lang" :to="lang">
-        <b-card :title="lang"></b-card>
+        <b-card class="lang-card" :title="lang">
+        	<img class="lang-img" :src="'https://res.cloudinary.com/dck5rpuat/image/upload/v1535347620/letters/' + lang.toLowerCase().trim() +'.png'">
+        </b-card>
       </nuxt-link>
     </b-col>
   </b-row>
   </transition>
 </template>
+
+<script>
+export default{
+	computed: {
+	  searchInLowerCase() {
+	    return this.search.toLowerCase().trim();
+	  }
+	}
+}
+</script>
+
+<style lang="scss">
+.lang-card{
+	height: 150px;
+	.card-body{
+		position:relative;
+
+		.lang-img{
+			position:absolute;
+	     	right: 20px;
+    		top: 20px;
+    		opacity: 0.5;
+		}
+		.card-title{
+			text-align: left;
+			margin-top:45px;
+		}
+	}	
+}
+</style>
