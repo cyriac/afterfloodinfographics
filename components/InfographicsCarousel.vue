08c694526bbdcc1fa2a461440a368ecc8b980c06
Anoop Sethumadhavan <anoop4sethu@gmail.com>
2018-09-02 21:00:41 +0530

    Configured carousel indicators to show up on card hover

--- components/InfographicsCarousel.vue
+++ components/InfographicsCarousel.vue
@@ -9,9 +9,6 @@
         <b-carousel-slide v-for="(png, index) in images"
                           :key="index"
                           :img-src="getimgurl(png)" />
-        <b-carousel-slide v-for="(png, index) in images"
-                          :key="'asd'+index"
-                          :img-src="getimgurl(png)" />
     </b-carousel>
     <b-img :src="getimgurl(images[0])" class="full-width-image" v-else/>
   </div>
@@ -35,3 +32,51 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .carousel-inner img{
+    filter: grayscale(100%);
+    border-radius: 0px;
+  }
+  .carousel-control-prev, .carousel-control-next{
+    opacity: 0;
+  }
+  .carousel:hover .carousel-control-prev, .carousel:hover .carousel-control-next{
+    opacity: 1;
+  }
+  .carousel-control-next-icon, .carousel-control-prev-icon {
+    width: 40px;
+    height: 40px;
+    background: rgba(0, 0, 0, .6);
+    border-radius: 50%;
+    position: relative;
+    padding: 9px;
+    text-align: center;
+  }
+  .carousel-control-next-icon:after{
+    position: absolute;
+    content: '';
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23fff' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E");
+
+
+    background-size: 15px 15px;
+    height: 15px;
+    width: 15px;
+    left: 14px;
+    top: 12px;
+    background-repeat: no-repeat;
+    margin: 0 auto;
+  }
+  .carousel-control-prev-icon:after {
+    position: absolute;
+    content: '';
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23fff' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
+    background-size: 15px 15px;
+    height: 15px;
+    width: 15px;
+    left: 12px;
+    top: 12px;
+    background-repeat: no-repeat;
+    margin: 0 auto;
+  }
+</style>
